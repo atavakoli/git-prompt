b97c56e70ba5dc40821af6b6796636bffa149416
Ali Tavakoli <ali.tavakoli@gmail.com>
2018-03-12 16:55:59 -0400

    Rewrite removing gawk dependency & improving speed.
    The use of `git status -b --porcelain` was making the prompt
    unecessarily slow, especially for large repos. This was replaced
    with various other git plumbing commands.
    
    Because of the above, gawk was no longer needed.

--- README.md
+++ README.md
@@ -2,98 +2,91 @@
 
 Bash prompt for Git
 
-The main file in this project is the ``git-prompt.sh`` file, which is a
-bash script that defines a function called ``git_prompt``, which outputs
+The main file in this project is the `git-prompt.sh` file, which is a
+bash script that defines a function called `git_prompt`, which outputs
 a small string showing the status of your Git repository, including the
 current branch/head, ahead/behind status, and working directory status.
 
-It's used by default to generate a custom bash prompt whenever you're in
-a Git-managed directory.
-
 ## Installation
 
-1. Copy ``git-prompt.sh`` into your home directory
-2. Add ``source ~/git-prompt.sh`` to your ``~/.bashrc`` file (or your
-   ``~/.bash_aliases`` file, as your prefer)
-3. Run ``source ~/.bashrc``
+1. `source ~/path/to/git-prompt.sh` in `~/.bashrc` or `~/.bash_profile`.
+2. Add `export PROMPT_COMMAND=...` with `$(git_prompt)` called somewhere
+   within an embedded export of `PS1`.
 
-## Dependencies
+For example:
 
-- ``gawk``
-
-  To parse the output of ``git status --porcelain`` to build the prompt.
+```
+source /path/to/git-prompt/git-prompt.sh
+export PROMPT_COMMAND="$PROMPT_COMMAND;"'export PS1="\h:\W \u$(git_prompt)\$"'
+```
 
 ## Design
 
-The ``git_prompt`` function will return a string showing the current branch
-name as well as how ahead/behind it is from its origin (since the last fetch).
-It will also show (via * before the branch name) whether the working
-directory is modified, and (via * after branch name) whether the index
+The `git_prompt` function will return a string showing the current branch
+name as well as how ahead/behind it is from its upstream since the last fetch.
+It will also show (via `*` before the branch name) whether the working
+directory is modified, and (via `*` after branch name) whether the index
 is modified. Finally, if you are on a detached HEAD, it will attempt to
-to show the tag, or the SHA-1 if the HEAD is not tagged.
+to show the tag, branch, path to closest ancestor, or SHA-1 of the commit.
 
-``git_prompt`` will return an empty string if you are not in a Git-managed
-directory (i.e. if the 'git status' command would fail).
-
-This function is used by default in the ``PROMPT_COMMAND`` environment
-variable, which in turn sets the ``PS1`` environment variable to a value
-containing the output of ``git_prompt``.  You can edit the version that
-comes in ``git-prompt.sh`` to match your preferred prompt format.
+`git_prompt` will return an empty string if you are not in a Git-managed
+directory (i.e. if the 'git status' command would fail). Otherwise, a
+bolded & colored string with 1 space on either side will be output.
 
 ## Examples
 
-``[master]``
+`<master>`
 
-This means that you are currently on the master branch in this git-managed
-directory, that you are neither ahead nor behind from master's origin,
-and that you have no changes in neither your working directory nor index.
+You are currently on a branch named master, which has no upstream.
 
-``[*master]``
+`[master]`
 
-This means that you are currently on the master branch in this git-managed
-directory, that you are neither ahead nor behind from master's origin,
-and that you do have changes in your working directory that are unstaged.
+You are currently on the master branch, you are neither ahead nor behind
+from master's upstream, and you have no changes in neither your working
+directory nor index.
 
-``[master*]``
+`[*master]`
 
-This means that you are currently on the master branch in this git-managed
-directory, that you are neither ahead nor behind from master's origin,
-and that you do have changes in your index that are uncommited.
+You are currently on the master branch, you are neither ahead nor behind
+from master's upstream, and you have changes in your working directory
+that are unstaged.
 
-``[*master*]``
+`[master*]`
 
-This means that you are currently on the master branch in this git-managed
-directory, that you are neither ahead nor behind from master's origin,
-and that you do have changes in both unstaged changes in your working
-directory and uncommitted changes in your index.
+You are currently on the master branch, you are neither ahead nor behind
+from master's upstream, and you have changes staged to be committed.
 
-``[3 -> foo]``
+`[*master*]`
 
-This means that you are currently on the foo branch, it is 3 ahead of its
-origin (i.e. you can push it).
+You are currently on the master branch, you are neither ahead nor behind
+from master's upstream, and you have changes in your working directory
+that are unstaged as well as changes staged to be committed.
 
-``[bar -> 2]``
+`[3 -> master]`
 
-This means that you are currently on the bar branch, it is 2 behind of its
-origin (i.e. you can fast-forward it).
+You are currently on the master branch, which is 3 ahead of its upstream
+(i.e. you can push it).
 
-``[3 -> foobar -> 2]``
+`[master -> 2]`
 
-This means that the foobar branch is 3 commits ahead and 2 commits behind
-of its origin (i.e. it's diverged).
+You are currently on the master branch, which is 2 behind of its upstream
+(i.e. you can fast-forward it).
 
-``[? -> foobar -> ?]``
+`[3 -> master -> 2]`
 
-This means that foobar branch has an upstream that is gone, requiring a
-`git branch --unset-upstream` to repair.
+You are currently on the master branch, which is 3 ahead and 2 behind
+of its upstream (i.e. it's diverged).
 
-``[detached(1.1.3)]``
+`<detached(1.1.3)>`
 
-This means that you are currently in detached HEAD mode, and HEAD points
-to tag 1.1.3.
+You are currently in detached HEAD mode, and HEAD points to tag 1.1.3.
 
-``[detached(1aaf71b)]``
+`<detached(master~1)>`
 
-This means that you are currently in detached HEAD mode, and HEAD is at
-commit 1aaf71b, which is not tagged.
+You are currently in detached HEAD mode, and HEAD points to the first
+parent of master.
 
+`<detached(1aaf71b)>`
+
+You are currently in detached HEAD mode, and HEAD points tocommit
+1aaf71b, which not a descendent of any named head.
--- git-prompt.sh
+++ git-prompt.sh
@@ -12,7 +12,10 @@
 # It will also show (via * before the branch name) whether the working
 # directory is modified, and (via * after branch name) whether the index
 # is modified. Finally, if you are on a detached HEAD, it will attempt to
-# to show the tag, or the SHA-1 if the HEAD is not tagged.
+# to show a tag, a branch, a path to the closest named ancestor, or a SHA-1.
+#
+# If on a branch with no upstream, or on a detached head, the prompt will use
+# angular brackets; otherwise, it will use square brackets.
 #
 # git_prompt will return an empty string if you are not in a git-managed
 # directory (i.e. if the 'git status' command would fail).
@@ -21,139 +24,111 @@
 #
 # EXAMPLES
 #
+#   <master>
+#
+#    You are currently on a branch named master, which has no upstream.
+#
 #   [master]
 #
-#    This means that you are currently on the master branch in this git-managed
-#    directory, that you are neither ahead nor behind from master's origin,
-#    and that you have no changes in neither your working directory nor index.
+#    You are currently on the master branch, you are neither ahead nor behind
+#    from master's upstream, and you have no changes in neither your working
+#    directory nor index.
 #
 #   [*master]
 #
-#    This means that you are currently on the master branch in this git-managed
-#    directory, that you are neither ahead nor behind from master's origin,
-#    and that you do have changes in your working directory that are unstaged.
+#    You are currently on the master branch, you are neither ahead nor behind
+#    from master's upstream, and you have changes in your working directory
+#    that are unstaged.
 #
 #   [master*]
 #
-#    This means that you are currently on the master branch in this git-managed
-#    directory, that you are neither ahead nor behind from master's origin,
-#    and that you do have changes in your index that are uncommited.
+#    You are currently on the master branch, you are neither ahead nor behind
+#    from master's upstream, and you have changes staged to be committed.
 #
 #   [*master*]
 #
-#    This means that you are currently on the master branch in this git-managed
-#    directory, that you are neither ahead nor behind from master's origin,
-#    and that you do have changes in both unstaged changes in your working
-#    directory and uncommitted changes in your index.
+#    You are currently on the master branch, you are neither ahead nor behind
+#    from master's upstream, and you have changes in your working directory
+#    that are unstaged as well as changes staged to be committed.
 #
-#   [3 -> foo]
+#   [3 -> master]
 #
-#    This means that you are currently on the foo branch, it is 3 ahead of its
-#    origin (i.e. you can push it).
+#    You are currently on the master branch, which is 3 ahead of its upstream
+#    (i.e. you can push it).
 #
-#   [bar -> 2]
+#   [master -> 2]
 #
-#    This means that you are currently on the bar branch, it is 2 behind of its
-#    origin (i.e. you can fast-forward it).
+#    You are currently on the master branch, which is 2 behind of its upstream
+#    (i.e. you can fast-forward it).
 #
-#   [3 -> foobar -> 2]
+#   [3 -> master -> 2]
 #
-#    This means that the foobar branch is 3 commits ahead and 2 commits behind
-#    of its origin (i.e. it's diverged).
+#    You are currently on the master branch, which is 3 ahead and 2 behind
+#    of its upstream (i.e. it's diverged).
 #
-#   [? -> foobar -> ?]
+#   <detached(1.1.3)>
 #
-#    This means that foobar branch has an upstream that is gone, requiring a
-#    'git branch --unset-upstream' to repair.
+#    You are currently in detached HEAD mode, and HEAD points to tag 1.1.3.
 #
-#   [detached(1.1.3)]
+#   <detached(master~1)>
 #
-#    This means that you are currently in detached HEAD mode, and HEAD points
-#    to tag 1.1.3.
+#    You are currently in detached HEAD mode, and HEAD points to the first
+#    parent of master.
 #
-#   [detached(1aaf71b)]
+#   <detached(1aaf71b)>
 #
-#    This means that you are currently in detached HEAD mode, and HEAD is at
-#    commit 1aaf71b, which is not tagged.
+#    You are currently in detached HEAD mode, and HEAD points tocommit
+#    1aaf71b, which not a descendent of any named head.
 #
 
-function git_prompt_format() {
-  if [ "$1" == 0 ]; then
+function git_prompt() {
+  if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
     return
   fi
 
-  echo -n " ["
-  if [ "$2" != 0 ]; then
-    echo -n "${2} -> "
-  fi
+  local c_red='\[\e[31m\]'
+  local c_green='\[\e[32m\]'
+  local c_bold='\[\e[1m\]'
+  local c_clear='\[\e[0m\]'
 
-  if [ "$4" != 0 ]; then
-    echo -n "*"
-  fi
+  local branch="$(git rev-parse --abbrev-ref HEAD 2>/dev/null)"
+  local counts
+  local unstaged
+  local staged
 
-  if [ "$1" == "DETACHEDHEAD" ]; then
-    echo -n "detached ($(git describe --always --tags HEAD 2> /dev/null))"
+  if [ -z "$branch" -o "$branch" == "HEAD" ]; then
+    color="$c_red"
+    branch="<detached($(git describe --all --contains --always HEAD))>"
   else
-    echo -n $1
+    color="$c_green"
+
+    git diff --no-ext-diff --quiet || unstaged='*'
+    git diff --no-ext-diff --quiet --staged || staged='*'
+
+    branch="$unstaged$branch$staged"
+
+    read ahead behind < <(git rev-list --count --left-right @{u}... 2>/dev/null)
+
+    if [ "$ahead$behind" == '' ]; then
+      branch="<$branch>"
+    else
+      if [ -n "$behind" ] && [ $behind -gt 0 ]; then
+        branch="$behind -> $branch"
+      fi
+      if [ -n "$ahead" ] && [ $ahead -gt 0 ]; then
+        branch="$branch -> $ahead"
+      fi
+
+      branch="[$branch]"
+    fi
   fi
 
-  if [ "$5" != 0 ]; then
-    echo -n "*"
-  fi
-
-  if [ "$3" != 0 ]; then
-    echo -n " -> ${3}"
-  fi
-
-  echo -n "]"
-}
-
-function git_prompt() {
-  export GIT_PROMPT_PARAM="$(git status -b --porcelain 2> /dev/null | gawk '
-    BEGIN {
-      workingTreeCount = 0;
-      indexCount = 0;
-    }
-
-    /^## .*$/ {
-      if (0 != match($0, /^## HEAD \(no branch\)$/)) {
-        printf "DETACHEDHEAD 0 0 ";
-      } else if (0 != match($0, /^## ([^ ]+)\.\.\.[^ ]* \[gone\]$/, groups)) {
-        printf "%s ? ? ", groups[1];
-      } else if (0 != match($0, /^## ([^ ]+)\.\.\.[^ ]*( \[(ahead ([0-9]+)(, )?)?(behind ([0-9]+))?\])?$/, groups)) {
-        printf "%s %d %d ", groups[1], (groups[4]"" == "" ? "0" : groups[4]), (groups[7]"" == "" ? "0" : groups[7]);
-      } else if (0 != match($0, /^## ([^ ]+)$/, groups)) {
-        printf "%s 0 0 ", groups[1];
-      }
-    }
-
-    /^ [A-Z].*$/ {
-      workingTreeCount +=1;
-    }
-
-    /^[A-Z] .*$/ {
-      indexCount +=1;
-    }
-
-    /^[A-Z]{2}.*$/ {
-      indexCount +=1;
-      workingTreeCount +=1;
-    }
-
-    END {
-      print workingTreeCount, indexCount;
-    }
-  ')"
-
-  if [ $? -eq 0 ]; then
-    git_prompt_format $GIT_PROMPT_PARAM
-  fi
+  echo -n " $c_bold$color$branch$c_clear "
 }
 
 # MODIFY THE BELOW TO ADD $(git_prompt) WHEREVER YOU WANT IN YOUR PS1
 # BUT MAKE SURE YOU ARE EXPORTING PS1 USING THE 'PROMPT_COMMAND' ENV VARIABLE
 #
-# THE BELOW IS SUGGESTED BASED ON CYGWIN'S DEFAULT PS1
+# FOR EXAMPLE:
 #
-export PROMPT_COMMAND='export PS1="\[\e]0;\w\a\]\n\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\]$(git_prompt)\n\$"'
-
+# export PROMPT_COMMAND="$PROMPT_COMMAND;"'export PS1="\h:\W \u$(git_prompt)\$"'
