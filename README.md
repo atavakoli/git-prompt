75ff5bd6d4dccbd60bdea3a0957c9d4660bb338d
Ali Tavakoli <ali.tavakoli@gmail.com>
2018-03-13 11:19:12 -0400

    Add merge, rebase, untracked, and env variables
    This version also fixes modified/staged asterisks not being shown
    in detached head mode.

--- README.md
+++ README.md
@@ -26,12 +26,26 @@ The `git_prompt` function will return a string showing the current branch
 name as well as how ahead/behind it is from its upstream since the last fetch.
 It will also show (via `*` before the branch name) whether the working
 directory is modified, and (via `*` after branch name) whether the index
-is modified. Finally, if you are on a detached HEAD, it will attempt to
-to show the tag, branch, path to closest ancestor, or SHA-1 of the commit.
+is modified. If you are rebasing or merging, it will indicate it by using
+rebase(...) or merge(...) around of the branch/commit name. Finally, if you
+are on a detached HEAD, it will attempt to show the tag, branch, path to
+closest ancestor, or SHA-1 of the commit.
 
 `git_prompt` will return an empty string if you are not in a Git-managed
 directory (i.e. if the 'git status' command would fail). Otherwise, a
 bolded & colored string with 1 space on either side will be output.
+
+## Environment Variables
+
+By default, git_prompt will bold & colorize its output (red for
+rebase/merge/detached, and green for everything else). To disable this,
+set `GITPROMPT_NOCOLOR` to any non-empty value.
+
+By default, untracked files are not shown because of poor performance in
+large repositories. To enable it, which will put a '+' to the left of the
+branch name if there are any untracked files, set `GITPROMPT_SHOW_UNTRACKED`
+to any non-empty value.
+
 
 ## Examples
 
@@ -77,6 +91,12 @@ You are currently on the master branch, which is 2 behind of its upstream
 You are currently on the master branch, which is 3 ahead and 2 behind
 of its upstream (i.e. it's diverged).
 
+`[*merge(master)*]`
+
+You are currently merging into the master branch, and you have both
+staged changes (e.g. resolved conflicts) and unstaged changes (e.g.
+unresolved conflicts).
+
 `<detached(1.1.3)>`
 
 You are currently in detached HEAD mode, and HEAD points to tag 1.1.3.
@@ -88,5 +108,9 @@ parent of master.
 
 `<detached(1aaf71b)>`
 
-You are currently in detached HEAD mode, and HEAD points tocommit
+You are currently in detached HEAD mode, and HEAD points to commit
 1aaf71b, which not a descendent of any named head.
+
+`<rebase(1aaf71b)>`
+
+You are currently rebasing, and HEAD points to commit 1aaf71b.
--- git-prompt.sh
+++ git-prompt.sh
@@ -11,8 +11,10 @@
 # as well as how ahead/behind it is from its origin (since the last fetch).
 # It will also show (via * before the branch name) whether the working
 # directory is modified, and (via * after branch name) whether the index
-# is modified. Finally, if you are on a detached HEAD, it will attempt to
-# to show a tag, a branch, a path to the closest named ancestor, or a SHA-1.
+# is modified. If you are rebasing or merging, it will indicate it by using
+# rebase(...) or merge(...) around of the branch/commit name. Finally, if you
+# are on a detached HEAD, it will attempt to show a tag, a branch, a path to the
+# closest named ancestor, or a SHA-1.
 #
 # If on a branch with no upstream, or on a detached head, the prompt will use
 # angular brackets; otherwise, it will use square brackets.
@@ -20,7 +22,16 @@
 # git_prompt will return an empty string if you are not in a git-managed
 # directory (i.e. if the 'git status' command would fail).
 #
+# ENVIRONMENT
 #
+# By default, git_prompt will bold & colorize its output (red for
+# rebase/merge/detached, and green for everything else). To disable this,
+# set GITPROMPT_NOCOLOR to any non-empty value.
+#
+# By default, untracked files are not shown because of poor performance in
+# large repositories. To enable it, which will put a '+' to the left of the
+# branch name if there are any untracked files, set GITPROMPT_SHOW_UNTRACKED
+# to any non-empty value.
 #
 # EXAMPLES
 #
@@ -66,6 +77,12 @@
 #    You are currently on the master branch, which is 3 ahead and 2 behind
 #    of its upstream (i.e. it's diverged).
 #
+#   [*merge(master)*]
+#
+#    You are currently merging into the master branch, and you have both
+#    staged changes (e.g. resolved conflicts) and unstaged changes (e.g.
+#    unresolved conflicts).
+#
 #   <detached(1.1.3)>
 #
 #    You are currently in detached HEAD mode, and HEAD points to tag 1.1.3.
@@ -77,8 +94,12 @@
 #
 #   <detached(1aaf71b)>
 #
-#    You are currently in detached HEAD mode, and HEAD points tocommit
+#    You are currently in detached HEAD mode, and HEAD points to commit
 #    1aaf71b, which not a descendent of any named head.
+#
+#   <rebase(1aaf71b)>
+#
+#    You are currently rebasing, and HEAD points to commit 1aaf71b.
 #
 
 function git_prompt() {
@@ -86,41 +107,72 @@ function git_prompt() {
     return
   fi
 
-  local c_red='\[\e[31m\]'
-  local c_green='\[\e[32m\]'
-  local c_bold='\[\e[1m\]'
-  local c_clear='\[\e[0m\]'
+  local c_red
+  local c_green
+  local c_bold
+  local c_clear
+
+  if [ -z "${GITPROMPT_NOCOLOR}" ]; then
+    c_red='\[\e[31m\]'
+    c_green='\[\e[32m\]'
+    c_bold='\[\e[1m\]'
+    c_clear='\[\e[0m\]'
+  fi
 
   local branch="$(git rev-parse --abbrev-ref HEAD 2>/dev/null)"
+  local gitdir="$(git rev-parse --git-path .)"
+  local label
   local counts
   local unstaged
   local staged
+  local untracked
+  local noupstream
+  local ahead
+  local behind
 
-  if [ -z "$branch" -o "$branch" == "HEAD" ]; then
+  if [ -z "$branch" ] || [ $branch == HEAD ]; then
+    if [ -d "$gitdir/rebase-merge" ] || [ -d "$gitdir/rebase-apply" ]; then
+      label=rebase
+    else
+      label=detached
+    fi
+
     color="$c_red"
-    branch="<detached($(git describe --all --contains --always HEAD))>"
+    branch="$label($(git describe --all --contains --always HEAD))"
+    noupstream=t
   else
     color="$c_green"
-
-    git diff --no-ext-diff --quiet || unstaged='*'
-    git diff --no-ext-diff --quiet --staged || staged='*'
-
-    branch="$unstaged$branch$staged"
-
-    read ahead behind < <(git rev-list --count --left-right @{u}... 2>/dev/null)
-
-    if [ "$ahead$behind" == '' ]; then
-      branch="<$branch>"
-    else
-      if [ -n "$behind" ] && [ $behind -gt 0 ]; then
-        branch="$behind -> $branch"
-      fi
-      if [ -n "$ahead" ] && [ $ahead -gt 0 ]; then
-        branch="$branch -> $ahead"
-      fi
-
-      branch="[$branch]"
+    if [ -f "$gitdir/MERGE_HEAD" ]; then
+      branch="merge($branch)"
     fi
+    read ahead behind < <(git rev-list --count --left-right @{u}... 2>/dev/null)
+  fi
+
+  git diff --no-ext-diff --quiet || unstaged='*'
+  git diff --no-ext-diff --quiet --staged || staged='*'
+
+  [ -n "$GITPROMPT_SHOW_UNTRACKED" ] \
+    && git ls-files --others --directory --no-empty-directory --error-unmatch \
+                    -- ':/*' >/dev/null 2>&1 \
+    && untracked='+'
+
+  branch="$untracked$unstaged$branch$staged"
+
+  if [ "$ahead$behind" == '' ]; then
+    noupstream=t
+  else
+    if [ -n "$behind" ] && [ $behind -gt 0 ]; then
+      branch="$behind -> $branch"
+    fi
+    if [ -n "$ahead" ] && [ $ahead -gt 0 ]; then
+      branch="$branch -> $ahead"
+    fi
+  fi
+
+  if [ -n "$noupstream" ]; then
+    branch="<$branch>"
+  else
+    branch="[$branch]"
   fi
 
   echo -n " $c_bold$color$branch$c_clear "
